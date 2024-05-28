709da3e7fed6e5dac5487056c2c92413ef2edca9
brajithdr <brajithdr@gmail.com>
2024-05-28 14:34:55 +0530

    First Version
    First Version

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function showLoveMessage() {
+    const message = "Shruthi, you are the love of my life. Every moment with you is a treasure. I can't wait to spend forever with you. ❤️";
+    document.getElementById('loveMessage').innerText = message;
+}
